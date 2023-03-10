2b7a9944909cc2df1b9cbe19efceebb1c3e9b378
Patrick Muth <marinermuph@gmail.com>
2023-03-09 19:04:20 -0800

    working YouTube API

---
+++ script.js
@@ -0,0 +1,43 @@
+let btnSearch = document.querySelector("#button-addon3");
+
+const searchApi = btnSearch.addEventListener("click", function () {
+
+  let searchQuery = document.querySelector("input[type = 'search']").value.trim();
+  searchQuery = searchQuery.toLowerCase().replace(' ', '_');
+  const searchTerms = searchQuery = document.querySelector("input[type = 'search']").value.trim();;
+
+  const YOUTUBE_API_KEY = "AIzaSyAIqG_Rna2mkVuTMJMJzjh2yQxvgbgY5hw";
+
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=` + searchTerms + `&key=${YOUTUBE_API_KEY}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector(".ytplayer").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+});
+
+
+
+
+
+
+//const searchTerms = searchQuery = document.querySelector("input[type = 'search']").value.trim();;
+
+// const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+
+// const YOUTUBE_API_KEY = "AIzaSyAIqG_Rna2mkVuTMJMJzjh2yQxvgbgY5hw";
+
+// const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=`+searchTerms+`&key=${YOUTUBE_API_KEY}`;
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     document.querySelector(".ytplayer").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+// });
+
