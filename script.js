0884b91ba18a0abc7cc9475b8c419a403df13ac8
Seth Leininger <sethlein@gmail.com>
2023-03-13 19:43:01 -0700

    Adding Title

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const searchApi2 = btnSearch2.addEventListener("click", function () {
 
   let searchQuery = document.querySelector("input[type = 'search']").value.trim();
   searchQuery = searchQuery.toLowerCase().replace(' ', '_');
-  //const searchTerms = searchQuery = document.querySelector("input[type = 'search']").value.trim();;
 
-  // const searchTerms = searchQuery = document.querySelector("input[type = 'search']").value.trim();;
   const YOUTUBE_API_KEY = "AIzaSyAIqG_Rna2mkVuTMJMJzjh2yQxvgbgY5hw";
 
   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=` + searchQuery + `&key=${YOUTUBE_API_KEY}`;
